c9dd1ec8d8babf30f094e2bf357984eb746dd89d
Harshali-22 <harshalitrade@gmail.com>
2025-10-16 15:17:18 +0530

    updates

--- Sales_Inventory/src/test/java/packagetest/A.java
+++ Sales_Inventory/src/test/java/packagetest/A.java
@@ -17,9 +17,7 @@ public  void m1() throws EncryptedDocumentException, IOException {
 	System.out.println("Added for feature branch-1");
 	System.out.println("added for feature branch-2");
 	System.out.println("added for feature branch-3");
-	System.out.println("Hello :)");
-	System.out.println("Hello :)");
-	System.out.println("Hello3 :)");
-	System.out.println("Hello4 :)");
+	System.out.println("Hello1 :)");
+	
 }
 }
